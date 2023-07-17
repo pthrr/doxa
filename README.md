e3bf2c4a9017cc8cd02ff52ac824b0e1a7d4f4b5
pthrr <pthrr@posteo.de>
2023-07-17 04:31:54 +0200

    Added Ergogen config + Fixes
    What:
    
    Why:
    
    How:
    
    Tags:

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# split-keyboard
+# doxa
+
+-
--- firmware/src/main.zig
+++ firmware/src/main.zig
@@ -1,20 +1,24 @@
 const std = @import("std");
-const microzig = @import("microzig");
-const rp2040 = microzig.hal;
-const time = rp2040.time;
-
-const pin_config = rp2040.pins.GlobalConfiguration{
-    .GPIO25 = .{
-        .name = "led",
-        .direction = .out,
-    },
-};
 
 pub fn main() !void {
-    const pins = pin_config.apply();
+    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
-    while (true) {
-        pins.led.toggle();
-        time.sleep_ms(250);
-    }
+    // stdout is for the actual output of your application, for example if you
+    // are implementing gzip, then only the compressed bytes should be sent to
+    // stdout, not any debugging messages.
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+
+    try bw.flush(); // don't forget to flush!
+}
+
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
